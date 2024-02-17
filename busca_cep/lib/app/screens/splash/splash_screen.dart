5c116810cf311f81f9d1591ca8081de244cb611c
Yuri Lima <yurilima742@yahoo.com>
2024-02-17 19:36:40 -0300

    refactor: Splash implementada e imagens adicionadas

--- busca_cep/lib/app/screens/splash/splash_screen.dart
+++ busca_cep/lib/app/screens/splash/splash_screen.dart
@@ -6,10 +6,51 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('teste'),
-      ),
-      body: Container()
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 0, 16, 20),
+
+          child: LayoutBuilder(
+            builder: (_, constraints) => SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                         height: constraints.maxHeight * 0.4,
+                      ),
+
+                      Image.asset(
+                        'assets/images/app_logo.png',
+                        width: 255,
+                        height: 53,
+                      ),
+
+                      const Spacer(),
+                          
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Pesquisa por Endereço'),
+                      ),
+                          
+                      const SizedBox(
+                         height: 20,
+                      ),
+                  
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Busca por CEP'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
     );
   }
 }
