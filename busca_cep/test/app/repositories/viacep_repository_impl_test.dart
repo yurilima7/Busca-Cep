4f93aa97f111a2afcc5c65b0ebd187005a57b8c9
Yuri Lima <yurilima742@yahoo.com>
2024-02-19 19:11:26 -0300

    refactor: fazendo algumas alterações no teste da api

--- busca_cep/test/app/repositories/viacep_repository_impl_test.dart
+++ busca_cep/test/app/repositories/viacep_repository_impl_test.dart
@@ -1,12 +1,15 @@
 import 'package:busca_cep/app/core/exception/repository_exception.dart';
-import 'package:busca_cep/app/core/rest_client/custom_dio.dart';
 import 'package:busca_cep/app/models/address_model.dart';
 import 'package:busca_cep/app/repositories/viacep_repository_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockViacepRepositoryImpl extends Mock implements ViacepRepositoryImpl {}
 
 void main() {
-  late ViacepRepositoryImpl viacepRepository;
-  late CustomDio customDio;
+  late MockViacepRepositoryImpl mockRepositoryImpl;
+  late AddressModel mockAddress;
+  late List<AddressModel> mockListAddress;
 
   const String cep = '65604556';
   const String uf = 'MA';
@@ -14,34 +17,81 @@ void main() {
   const String city = 'Caxias';
 
   setUp(() {
-    customDio = CustomDio();
-    viacepRepository = ViacepRepositoryImpl(dio: customDio);
+    mockRepositoryImpl = MockViacepRepositoryImpl();
+
+    mockAddress = AddressModel(
+      cep: '01001000',
+      city: 'Caxias',
+      complement: '',
+      ddd: '99',
+      ibge: '',
+      neighborhood: '',
+      street: 'Rua Teste',
+      uf: 'MA',
+    );
+
+    mockListAddress = [
+      AddressModel(
+        cep: '01001000',
+        city: 'Caxias',
+        complement: '',
+        ddd: '99',
+        ibge: '',
+        neighborhood: '',
+        street: 'Rua Teste',
+        uf: 'MA',
+      ),
+
+      AddressModel(
+        cep: '01001000',
+        city: 'Caxias',
+        complement: '',
+        ddd: '99',
+        ibge: '',
+        neighborhood: '',
+        street: 'Rua Teste',
+        uf: 'MA',
+      ),
+    ];
   });
 
   group('API Viacep', () {
-    test('Deve retornar um endereço da api', () async {
-      final address = await viacepRepository.findAddress(cep);
+    test('Deve retornar um endereço', () async {
+      when(() => mockRepositoryImpl.findAddress(any()))
+          .thenAnswer((_) async => mockAddress);
+
+      final address = await mockRepositoryImpl.findAddress(cep);
 
       expect(address, isA<AddressModel>());
     });
 
-    test('Deve retornar uma lista de endereços da api', () async {
+    test('Deve retornar uma lista de endereços', () async {
+      when(() => mockRepositoryImpl.findAllAddress(any(), any(), any()))
+          .thenAnswer((_) async => mockListAddress);
+
       final listAddress =
-          await viacepRepository.findAllAddress(uf, city, street);
+          await mockRepositoryImpl.findAllAddress(uf, city, street);
 
       expect(listAddress, isA<List<AddressModel>>());
     });
 
     test('Deve retornar um erro ao buscar um endereço por cep', () async {
+      when(() => mockRepositoryImpl.findAddress(any())).thenThrow(
+          RepositoryException(message: 'Erro ao buscar o endereço do cep!'));
+          
       expectLater(
-        () async => await viacepRepository.findAddress(''),
+        () async => await mockRepositoryImpl.findAddress(''),
         throwsA(isA<RepositoryException>()),
       );
     });
 
     test('Deve retornar um erro ao buscar uma lista de endereços', () async {
+      when(() => mockRepositoryImpl.findAllAddress(any(), any(), any())).thenThrow(
+          RepositoryException(
+              message: 'Erro ao buscar os endereços para visualizar o cep!'));
+
       expectLater(
-        () async => await viacepRepository.findAllAddress(uf, city, ''),
+        () async => await mockRepositoryImpl.findAllAddress(uf, city, ''),
         throwsA(isA<RepositoryException>()),
       );
     });
