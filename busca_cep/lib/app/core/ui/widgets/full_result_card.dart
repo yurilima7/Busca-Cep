d117fb014ddeb0c0dfdc3487692546b027bc58fd
Yuri Lima <yurilima742@yahoo.com>
2024-02-19 17:35:50 -0300

    refactor: Mostrando mensagem caso retorne um model vázio

--- busca_cep/lib/app/core/ui/widgets/full_result_card.dart
+++ busca_cep/lib/app/core/ui/widgets/full_result_card.dart
@@ -1,4 +1,5 @@
 import 'package:busca_cep/app/core/ui/styles/colors_app.dart';
+import 'package:busca_cep/app/core/ui/styles/text_app.dart';
 import 'package:busca_cep/app/models/address_model.dart';
 import 'package:flutter/material.dart';
 
@@ -9,36 +10,45 @@ class FullResultCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
+    return Visibility(
+      visible: addressModel?.neighborhood != '',
 
-      child: Card(
-        color: context.colors.light,
-
-        shape: RoundedRectangleBorder(
-          side: BorderSide(color: context.colors.primary, width: 2.0),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-
-            children: [
-              Text("Bairro: ${addressModel?.neighborhood ?? ''}",),
-              const SizedBox(height: 2),
-              Text("Rua: ${addressModel?.street ?? ''}"),
-              const SizedBox(height: 2),
-              Text("Cep: ${addressModel?.cep ?? ''}"),
-              const SizedBox(height: 2),
-              Text("Cidade: ${addressModel?.city ?? ''}"),
-              const SizedBox(height: 2),
-              Text("Complemento: ${addressModel?.complement ?? ''}"),
-              const SizedBox(height: 2),
-              Text("UF: ${addressModel?.uf ?? ''}"),
-            ],
+      replacement: Text(
+        'Retornou vazio, tente novamente.',
+        style: context.textApp.title,
+      ),
+      
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+      
+        child: Card(
+          color: context.colors.light,
+      
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: context.colors.primary, width: 2.0),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+      
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+      
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+      
+              children: [
+                Text("Bairro: ${addressModel?.neighborhood ?? ''}",),
+                const SizedBox(height: 2),
+                Text("Rua: ${addressModel?.street ?? ''}"),
+                const SizedBox(height: 2),
+                Text("Cep: ${addressModel?.cep ?? ''}"),
+                const SizedBox(height: 2),
+                Text("Cidade: ${addressModel?.city ?? ''}"),
+                const SizedBox(height: 2),
+                Text("Complemento: ${addressModel?.complement ?? ''}"),
+                const SizedBox(height: 2),
+                Text("UF: ${addressModel?.uf ?? ''}"),
+              ],
+            ),
           ),
         ),
       ),
